ce3c73344d970b7ea531d13dc35beb8d5b717032
Andresin96 <51256764+Andresin96@users.noreply.github.com>
2020-07-08 21:19:13 -0400

    Creo Nuevo Proyecto (Persistencia)

---
+++ Principal/Persistencia/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistencia
+{
+    public abstract class AbstractService<T>
+    {
+        public abstract int AddEntity(T entity);
+        public abstract int UpdateEntity(T entity);
+        public abstract T GetEntity(object key);
+        public abstract List<T> GetEntities();
+        public abstract void DeleteEntity(object key);
+    }
+}
