1151ce277c40736923c006d7b1c6b44856be8cfa
Andresin96 <51256764+Andresin96@users.noreply.github.com>
2020-07-10 02:26:07 -0400

    método guardar empleado

--- Principal/Principal/PantallaRegistroUsuario.xaml.cs
+++ Principal/Principal/PantallaRegistroUsuario.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Persistencia;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,11 +24,73 @@ namespace Principal
         public PantallaRegistroUsuario()
         {
             InitializeComponent();
+            CargarTipoCargo();
         }
 
         private void btnRegistrar_Click(object sender, RoutedEventArgs e)
         {
-            this.Close();
+            RegistrarEmpleado();
+            
+        }
+
+        private void RegistrarEmpleado()
+        {
+            if (txtNombre.Text == "" || txtApellido.Text == "" || txtUsuario.Text == "" || txtContraseña.Text == "")
+            {
+                MessageBox.Show("DEBES LLENAR TODOS LOS CAMPOS SOLICITADOS", "Mensaje", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+               
+                if (rbnConfirmacion.IsChecked == false)
+                {
+                    MessageBox.Show("DEBE CONFIRMAR LA INFORMACION", "Mensaje", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+
+                }
+                else
+                {
+
+                    EMPLEADO empleado = new EMPLEADO();
+                    {
+                        empleado.Nombre = txtNombre.Text;
+                        empleado.Apellido = txtApellido.Text;
+                        empleado.Usuario = txtUsuario.Text;
+                        empleado.Contraseña = txtContraseña.Text;
+                        empleado.idCargo = (int)cbxTipoUsuario.SelectedValue;
+                        empleado.CodVendedor = Numero();
+                    };
+                    ServiciosEmpleado servicio = new ServiciosEmpleado();
+                    servicio.AddEntity(empleado);
+
+                    MessageBox.Show("EMPLEADO REGISTRADO EXITOSAMENTE", "Mensaje", MessageBoxButton.OK, MessageBoxImage.Information);
+                    this.Close();
+
+                }
+            }
+        }
+
+        private void CargarTipoCargo()
+        {
+            ServiciosCargo servicio = new ServiciosCargo();
+            List<CARGO> tipos = servicio.GetEntities();
+            cbxTipoUsuario.ItemsSource = tipos;
+            cbxTipoUsuario.SelectedValuePath = "id";
+            cbxTipoUsuario.DisplayMemberPath = "Nombre";
+        }
+
+        public static int Numero()
+        {
+            using (SqlConnection cn = new SqlConnection("Data Source=ANDREEEEES\\SQLEXPRESS;Initial Catalog=LaTirana;Integrated Security=True"))
+            {
+                cn.Open();
+                string sql = "SELECT MAX(CodVendedor) FROM EMPLEADO";
+                SqlCommand cmd = new SqlCommand(sql, cn);
+
+                int MaxNumero = 0;
+                int.TryParse(cmd.ExecuteScalar().ToString(), out MaxNumero);
+
+                return MaxNumero + 1001;
+            }
         }
     }
 }
